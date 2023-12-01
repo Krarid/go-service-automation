14ed5d4b5671a8256251feb1822606e79ee47319
Javier Meléndez <jmz.krarid@outlook.com>
2023-12-01 12:42:42 -0600

    Fix test runner

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,8 +7,8 @@ import io.cucumber.testng.CucumberOptions;
 				plugin = {"pretty", "html:target/report/report.html", "json:target/report/cucu_json_report.json", "junit:target/report/cucumber_junit_report.xml",
 						"rerun:target/report/failed.txt"},
 				features = {"src/test/resources/steps"},
-				glue = {"hugo"},
-				tags = "@HUGO"
+				glue = {"steps"},
+				tags = "@Hugo"
 				)
 public class TestRunner extends AbstractTestNGCucumberTests {
 
--- src/test/java/steps/Test.java
+++ src/test/java/steps/Test.java
@@ -1,4 +1,4 @@
-package hugo;
+package steps;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
