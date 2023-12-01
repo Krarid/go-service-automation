879b4f5b9b02ed921324cca3013324ce551e85c8
Javier Meléndez <jmz.krarid@outlook.com>
2023-12-01 11:50:40 -0600

    Modify main class to run it as a Java application

--- src/test/java/hugo/Test.java
+++ src/test/java/hugo/Test.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Test {
-	public void main()
+	public static void main(String[] args)
 	{
 		WebDriver driver = WebDriverManager.chromedriver().create();
 		
